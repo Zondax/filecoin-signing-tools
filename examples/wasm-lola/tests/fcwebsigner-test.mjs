0507332ee3c001cc1e18da597eda894b9d1bfc44
rllola <rllola80@gmail.com>
2020-02-03 14:26:41 +0100

    Only test for ES modules (no framework)

---
+++ examples/wasm-lola/tests/fcwebsigner-test.mjs
@@ -0,0 +1,14 @@
+import * as assert from 'assert';
+import {hello} from 'fcwebsigner';
+
+test('Hello world', () => {
+  assert.equal(hello(), 123);
+})
+
+test('Hello world fail', () => {
+  assert.equal(hello(), 124);
+})
+
+test('Hello world good', () => {
+  assert.equal(hello(), 123);
+})
--- examples/wasm/tests/basic.ispec.js
+++ examples/wasm/tests/basic.ispec.js
@@ -2,6 +2,8 @@ import {expect, test} from "jest";
 import Transport from "@ledgerhq/hw-transport";
 import {hello} from "fcwebsigner";
 
+console.log(hello)
+
 test("get version", async () => {
   hello();
 });
