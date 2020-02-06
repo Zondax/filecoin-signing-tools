5969110c577c73e0a9eaf7ce61d107ddb022b5b8
rllola <rllola80@gmail.com>
2020-02-06 13:52:11 +0100

    Derive key from mnemonic and return public key in a Keypair instance;

--- examples/wasm/tests/fcwebsigner-test.mjs
+++ examples/wasm/tests/fcwebsigner-test.mjs
@@ -1,5 +1,5 @@
 import * as assert from 'assert';
-import {hello, verify_signature} from 'fcwebsigner';
+import {hello, verify_signature, key_generate, key_derive} from 'fcwebsigner';
 
 test('Hello world', () => {
     assert.equal(hello(), 123);
@@ -9,6 +9,24 @@ test('Hello world', () => {
 test('Hello world fail', () => {
     // assert.equal(hello(), 124);
 });
+
+test('Key Generate', () => {
+
+  // TODO
+  let key = key_generate();
+
+  console.log("Pubkey :", key.pubkey);
+  console.log("Prvkey :", key.prvkey);
+
+  assert.equal(key.pubkey, "Public key!")
+})
+
+test('Key Derive', () => {
+  let keypair = key_derive("equip will roof matter pink blind book anxiety banner elbow sun young", "m/44'/161'/0/0/1");
+
+  console.log("Pubkey :", keypair.pubkey);
+  console.log("Prvkey :", keypair.prvkey);
+})
 
 test('Verify signature', () => {
   assert.equal(verify_signature(), false);
--- fcwebsigner/src/lib.rs
+++ fcwebsigner/src/lib.rs
@@ -4,6 +4,10 @@ use crate::utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
 use fcsigner;
 
+use bip39::{Mnemonic, Language, Seed};
+use tiny_hderive::bip32::ExtendedPrivKey;
+use secp256k1::{SecretKey, PublicKey};
+
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -12,8 +16,30 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub struct Keypair {
-    pubkey: u8[32],
-    prvkey: u8[44],
+    // hexstring of the compressed public key
+    pubkey: String,
+    // hexstring of the private key
+    prvkey: String,
+    // Address in the string format
+    address: String
+}
+
+#[wasm_bindgen]
+impl Keypair {
+    #[wasm_bindgen(getter)]
+    pub fn pubkey(&self) -> String {
+        self.pubkey.clone()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn prvkey(&self) -> String {
+        self.prvkey.clone()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn address(&self) -> String {
+        self.address.clone()
+    }
 }
 
 #[wasm_bindgen]
@@ -23,9 +49,57 @@ pub fn hello() -> u8 {
 }
 
 #[wasm_bindgen]
-pub fn key_generate() {
+pub fn key_generate() -> Keypair {
     // TODO: return keypair (pub/priv + address)
-    fcsigner::key_generate();
+    // fcsigner::key_generate();
+
+    set_panic_hook();
+
+    let keypair = Keypair {
+        pubkey: String::from("Public key!"),
+        prvkey: String::from("Private key!"),
+        address: String::from("Address!")
+    };
+
+    return keypair;
+}
+
+#[wasm_bindgen]
+pub fn key_derive(_mnemonic: String, _path: String) -> Keypair {
+    // TODO mnemonic + path
+    // TODO: return keypair (pub/priv + address)
+
+    set_panic_hook();
+
+    let result_mnemonic = Mnemonic::from_phrase(_mnemonic.as_str(), Language::English);
+
+    match result_mnemonic {
+        Ok(mnemonic) => {
+            let seed = Seed::new(&mnemonic, "");
+            let ext = ExtendedPrivKey::derive(seed.as_ref(), _path.as_str()).unwrap();
+
+            let secret_key = SecretKey::parse(&ext.secret()).unwrap();
+            let public_key = PublicKey::from_secret_key(&secret_key);
+
+            let keypair = Keypair {
+                pubkey: utils::to_hex_string(&public_key.serialize_compressed()),
+                prvkey: utils::to_hex_string(&ext.secret()),
+                address: String::from("Address!")
+            };
+
+            return keypair;
+        }
+
+        Err(_) => {
+            let keypair = Keypair {
+                pubkey: String::from("Error!"),
+                prvkey: String::from("Error!"),
+                address: String::from("Error!")
+            };
+
+            return keypair;
+        }
+    }
 }
 
 #[wasm_bindgen]
