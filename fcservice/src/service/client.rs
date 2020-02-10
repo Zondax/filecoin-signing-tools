76f6e56f762e458d082492707346643d1e239ac3
Juan Leni <juan.leni@zondax.ch>
2020-02-10 21:18:50 +0100

    add test

--- fcservice/src/service/client.rs
+++ fcservice/src/service/client.rs
@@ -14,20 +14,25 @@ lazy_static! {
     static ref NONCE_CACHE: Mutex<LruCache::<String, u64>> = Mutex::new(LruCache::new(100));
 }
 
-fn get_cached_nonce(addr: &str) -> Option<u64> {
+fn cache_get_nonce(addr: &str) -> Option<u64> {
     // Retrieve from cache
     let mut cache = NONCE_CACHE.lock().expect("mutex lock failed");
     let nonce = cache.get(&addr.to_owned());
     nonce.and_then(|v| Some(*v)).or_else(|| None)
 }
 
-fn put_cached_nonce(addr: &str, nonce: u64) {
+fn cache_put_nonce(addr: &str, nonce: u64) {
     let mut cache = NONCE_CACHE.lock().expect("mutex lock failed");
     cache.put(addr.to_owned(), nonce);
 }
 
+fn cache_len() -> usize {
+    let mut cache = NONCE_CACHE.lock().expect("mutex lock failed");
+    cache.len()
+}
+
 pub async fn get_nonce(addr: &str) -> Result<u64, anyhow::Error> {
-    if let Some(nonce) = get_cached_nonce(addr) {
+    if let Some(nonce) = cache_get_nonce(addr) {
         return Ok(nonce);
     }
 
@@ -68,6 +73,30 @@ pub async fn get_nonce(addr: &str) -> Result<u64, anyhow::Error> {
         }
     };
 
-    put_cached_nonce(addr, nonce);
+    cache_put_nonce(addr, nonce);
     Ok(nonce)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::service::client::{cache_get_nonce, cache_len, cache_put_nonce};
+
+    #[test]
+    fn cache_put_get() {
+        let not_found = cache_get_nonce("unknown");
+        assert!(not_found.is_none());
+        assert_eq!(cache_len(), 0);
+
+        cache_put_nonce("address1", 123);
+        cache_put_nonce("address2", 456);
+        assert_eq!(cache_len(), 2);
+
+        let found1 = cache_get_nonce("address1");
+        assert!(found1.is_some());
+        assert_eq!(found1.unwrap(), 123);
+
+        let found2 = cache_get_nonce("address2");
+        assert!(found2.is_some());
+        assert_eq!(found2.unwrap(), 456);
+    }
 }
