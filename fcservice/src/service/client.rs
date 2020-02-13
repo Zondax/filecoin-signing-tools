29341a5bd8981f011fee8641cd4057a0adbd4dd7
Juan Leni <juan.leni@zondax.ch>
2020-02-13 23:18:27 +0100

    cache refactoring

--- fcservice/src/service/client.rs
+++ fcservice/src/service/client.rs
@@ -1,37 +1,14 @@
 ////! Fcservice RPC Client
 
+use crate::service::cache::{cache_get_nonce, cache_put_nonce};
 use crate::service::error::RemoteNode::{EmptyNonce, InvalidNonce};
 use crate::service::error::ServiceError;
 use jsonrpc_core::Result as CoreResult;
 use jsonrpc_core::{Id, MethodCall, Params, Response, Version};
-use lazy_static::lazy_static;
-use lru::LruCache;
 use serde_json::value::Value;
 use std::sync::atomic::{AtomicU64, Ordering};
-use std::sync::Mutex;
 
 static CALL_ID: AtomicU64 = AtomicU64::new(1);
-
-lazy_static! {
-    static ref NONCE_CACHE: Mutex<LruCache::<String, u64>> = Mutex::new(LruCache::new(100));
-}
-
-fn cache_get_nonce(addr: &str) -> Option<u64> {
-    // Retrieve from cache
-    let mut cache = NONCE_CACHE.lock().expect("mutex lock failed");
-    let nonce = cache.get(&addr.to_owned());
-    nonce.copied().or_else(|| None)
-}
-
-fn cache_put_nonce(addr: &str, nonce: u64) {
-    let mut cache = NONCE_CACHE.lock().expect("mutex lock failed");
-    cache.put(addr.to_owned(), nonce);
-}
-
-fn cache_len() -> usize {
-    let cache = NONCE_CACHE.lock().expect("mutex lock failed");
-    cache.len()
-}
 
 pub async fn get_nonce(addr: &str) -> Result<u64, ServiceError> {
     if let Some(nonce) = cache_get_nonce(addr) {
@@ -73,28 +50,4 @@ pub async fn get_nonce(addr: &str) -> Result<u64, ServiceError> {
 
     cache_put_nonce(addr, nonce);
     Ok(nonce)
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::service::client::{cache_get_nonce, cache_len, cache_put_nonce};
-
-    #[test]
-    fn cache_put_get() {
-        let not_found = cache_get_nonce("unknown");
-        assert!(not_found.is_none());
-        assert_eq!(cache_len(), 0);
-
-        cache_put_nonce("address1", 123);
-        cache_put_nonce("address2", 456);
-        assert_eq!(cache_len(), 2);
-
-        let found1 = cache_get_nonce("address1");
-        assert!(found1.is_some());
-        assert_eq!(found1.unwrap(), 123);
-
-        let found2 = cache_get_nonce("address2");
-        assert!(found2.is_some());
-        assert_eq!(found2.unwrap(), 456);
-    }
 }
