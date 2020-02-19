73db9e541d4fb998c43a375475cab6183b501408
rllola <rllola80@gmail.com>
2020-02-19 17:11:52 +0100

    rustfmt

--- fcwebsigner/src/lib.rs
+++ fcwebsigner/src/lib.rs
@@ -39,8 +39,13 @@ pub fn transaction_create(unsigned_message_string: String) -> Result<String, JsV
 pub fn transaction_parse(cbor_hexstring: String, network: bool) -> Result<String, JsValue> {
     set_panic_hook();
 
+<<<<<<< HEAD
     let message_parsed =
         fcsigner::transaction_parse(cbor_hexstring.as_bytes(), network).map_err(|e| JsValue::from(e.to_string()))?;
+=======
+    let message_parsed = fcsigner::transaction_parse(cbor_hexstring, network)
+        .map_err(|e| JsValue::from(e.to_string()))?;
+>>>>>>> rustfmt
 
     let tx = serde_json::to_string(&message_parsed).map_err(|e| JsValue::from(e.to_string()))?;
 
