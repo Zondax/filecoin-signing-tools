93a7f1083b7700784ab9c1bd105c94d0f90a3c5a
Juan Leni <juan.leni@zondax.ch>
2020-03-12 15:11:16 +0100

    reformatting file

--- examples/jsonrpc/tests/basic.ispec.js
+++ examples/jsonrpc/tests/basic.ispec.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-console */
 import { test, expect } from "jest";
-import { callMethod } from "../src";
 import * as bip32 from "bip32";
-import {getDigest} from './utils.js'
-import secp256k1 from 'secp256k1';
-import fs from 'fs';
+import secp256k1 from "secp256k1";
+import fs from "fs";
+import { getDigest } from "./utils.js";
+import { callMethod } from "../src";
 
 const tests_vectors_path = "../manual_testvectors.json";
 
@@ -13,21 +13,23 @@ const URL = "http://127.0.0.1:3030/v0";
 
 const mnemonic = "equip will roof matter pink blind book anxiety banner elbow sun young";
 
-const cbor_transaction = "885501fd1d0f4dfcd7e99afcb99a8326b7dc459d32c62855010f323f4709e8e4db0c1d4cd374f9f35201d26fb20144000186a0430009c4430061a80040";
+const cbor_transaction =
+  "885501fd1d0f4dfcd7e99afcb99a8326b7dc459d32c62855010f323f4709e8e4db0c1d4cd374f9f35201d26fb20144000186a0430009c4430061a80040";
 
 const transaction = {
-  "to": "t17uoq6tp427uzv7fztkbsnn64iwotfrristwpryy",
-  "from": "t1b4zd6ryj5dsnwda5jtjxj6ptkia5e35s52ox7ka",
-  "nonce": 1,
-  "value": "100000",
-  "gas_price": "2500",
-  "gas_limit": "25000",
-  "method": 0,
-  "params": ""
+  to: "t17uoq6tp427uzv7fztkbsnn64iwotfrristwpryy",
+  from: "t1b4zd6ryj5dsnwda5jtjxj6ptkia5e35s52ox7ka",
+  nonce: 1,
+  value: "100000",
+  gas_price: "2500",
+  gas_limit: "25000",
+  method: 0,
+  params: "",
 };
 
-const prv_root_key = "xprv9s21ZrQH143K49QgrAgAVELf6ue2tZNHYUc7yfj8JGZY9SpZ38u8EfhWi85GsA6grUeB36wXrbNTkjX9EfGP1ybbPRG4sdP2EPfY1SZ2BF5";
-let node = bip32.fromBase58(prv_root_key);
+const prv_root_key =
+  "xprv9s21ZrQH143K49QgrAgAVELf6ue2tZNHYUc7yfj8JGZY9SpZ38u8EfhWi85GsA6grUeB36wXrbNTkjX9EfGP1ybbPRG4sdP2EPfY1SZ2BF5";
+const node = bip32.fromBase58(prv_root_key);
 
 test("key_generate_mnemonic", async () => {
   const response = await callMethod(URL, "key_generate_mnemonic", [], 1);
@@ -42,8 +44,8 @@ test("key_generate_mnemonic", async () => {
 test("key_derive", async () => {
   const path = "m/44'/461'/0/0/0";
   const response = await callMethod(URL, "key_derive", [mnemonic, path], 1);
-  let child = node.derivePath(path);
-  console.log(response)
+  const child = node.derivePath(path);
+  console.log(response);
 
   // Do we have a results
   expect(response).toHaveProperty("result");
@@ -69,59 +71,51 @@ test("key_derive missing 1 parameters", async () => {
 });
 
 test("transaction_serialize", async () => {
-  const response = await callMethod(
-    URL,
-    "transaction_serialize",
-    transaction,
-    1,
-  );
+  const response = await callMethod(URL, "transaction_serialize", transaction, 1);
 
-  expect( Buffer.from(response.result).toString('hex')).toBe(cbor_transaction);
+  expect(Buffer.from(response.result).toString("hex")).toBe(cbor_transaction);
 });
 
 test("transaction_parse", async () => {
-  const response = await callMethod(
-    URL,
-    "transaction_parse",
-    [cbor_transaction, true],
-    1,
-  );
+  const response = await callMethod(URL, "transaction_parse", [cbor_transaction, true], 1);
 
   expect(response.result).toBe(JSON.stringify(transaction));
 });
 
 test("transaction_testvectors", async () => {
-  let rawData = fs.readFileSync(tests_vectors_path);
-  let jsonData = JSON.parse(rawData);
+  const rawData = fs.readFileSync(tests_vectors_path);
+  const jsonData = JSON.parse(rawData);
 
   for (let i = 0; i < jsonData.length; i += 1) {
-    let tc = jsonData[i];
+    const tc = jsonData[i];
     console.log(tc.message);
     if (!tc.message.params) {
-      tc.message["params"] = ""
+      tc.message.params = "";
     }
 
-    let response = await callMethod(URL, "transaction_serialize", tc.message, i);
+    const response = await callMethod(URL, "transaction_serialize", tc.message, i);
 
     if (response.error) {
       console.log("Error", response);
       expect(tc.valid).toEqual(false);
     } else {
-      console.log("Testcase ------------------------------------------------------------------------------------");
+      console.log(
+        "Testcase ------------------------------------------------------------------------------------",
+      );
       console.log(tc.description);
       console.log("Reply", response);
-      expect( Buffer.from(response.result).toString('hex') ).toEqual(tc.encoded_tx_hex);
+      expect(Buffer.from(response.result).toString("hex")).toEqual(tc.encoded_tx_hex);
     }
   }
 });
 
-let rawData = fs.readFileSync(tests_vectors_path);
-let jsonData = JSON.parse(rawData);
+const rawData = fs.readFileSync(tests_vectors_path);
+const jsonData = JSON.parse(rawData);
 
 for (let i = 0; i < jsonData.length; i += 1) {
-  let tc = jsonData[i];
+  const tc = jsonData[i];
   if (!tc.message.params) {
-    tc.message["params"] = "";
+    tc.message.params = "";
   }
 
   if (tc.not_implemented) {
@@ -129,26 +123,24 @@ for (let i = 0; i < jsonData.length; i += 1) {
     // FIXME: doesn't fail for empty value #54
     console.log("FIX ME: Protocol 0 parsing not implemented in forest");
     continue;
-  };
-
+  }
   // Create test case for each
-  test("Parse Transaction : " + tc.description, async () => {
-    let response = await callMethod(URL, "transaction_parse", [tc.encoded_tx_hex, tc.testnet], i);
+  test(`Parse Transaction : ${tc.description}`, async () => {
+    const response = await callMethod(URL, "transaction_parse", [tc.encoded_tx_hex, tc.testnet], i);
 
     if (tc.valid) {
       console.log(response);
       expect(JSON.parse(response.result)).toEqual(tc.message);
     } else {
       console.log(response.error);
-      expect(response).toHaveProperty('error');
+      expect(response).toHaveProperty("error");
     }
-
-  })
+  });
 }
 
 test("sign_transaction", async () => {
-  let child = node.derivePath("m/44'/461'/0/0/0");
-  let message_digest = getDigest(Buffer.from(cbor_transaction, 'hex'));
+  const child = node.derivePath("m/44'/461'/0/0/0");
+  const message_digest = getDigest(Buffer.from(cbor_transaction, "hex"));
 
   const response = await callMethod(
     URL,
@@ -157,29 +149,29 @@ test("sign_transaction", async () => {
     1,
   );
 
-  let signatureBuffer = Buffer.from(response.result, "hex").slice(0,-1);
+  const signatureBuffer = Buffer.from(response.result, "hex").slice(0, -1);
 
   // compare signature
-  let signatureCompare = secp256k1.ecdsaSign(message_digest, child.privateKey);
+  const signatureCompare = secp256k1.ecdsaSign(message_digest, child.privateKey);
 
   expect(Buffer.from(signatureCompare.signature)).toEqual(signatureBuffer);
 
   // Remove V from result to verify signature
-  let result = secp256k1.ecdsaVerify(signatureBuffer, message_digest, child.publicKey);
+  const result = secp256k1.ecdsaVerify(signatureBuffer, message_digest, child.publicKey);
 
   expect(result).toBeTruthy();
 });
 
 test("sign_invalid_transaction", async () => {
-  let child = node.derivePath("m/44'/461'/0/0/0");
-  let invalid_transaction = {
-    "to": "t17uoq6tp427uzv7fztkbsnn64iwotfrristwpryy",
-    "from": "t1xcbgdhkgkwht3hrrnui3jdopeejsoas2rujnkdi",
-    "value": "100000",
-    "gas_price": "2500",
-    "gas_limit": "25000",
-    "method": 0,
-    "params": ""
+  const child = node.derivePath("m/44'/461'/0/0/0");
+  const invalid_transaction = {
+    to: "t17uoq6tp427uzv7fztkbsnn64iwotfrristwpryy",
+    from: "t1xcbgdhkgkwht3hrrnui3jdopeejsoas2rujnkdi",
+    value: "100000",
+    gas_price: "2500",
+    gas_limit: "25000",
+    method: 0,
+    params: "",
   };
 
   const response = await callMethod(
@@ -190,27 +182,23 @@ test("sign_invalid_transaction", async () => {
   );
 
   // Verify we have an error message
-  expect(response).toHaveProperty('error');
+  expect(response).toHaveProperty("error");
   // Verify we have the corrcet error message 'missing nonce'
   expect(response.error.message).toMatch(/missing field `nonce`/);
-})
+});
 
 test("verify_signature", async () => {
-  let child = node.derivePath("m/44'/461'/0/0/0");
+  const child = node.derivePath("m/44'/461'/0/0/0");
 
-  let message_digest = getDigest(Buffer.from(cbor_transaction, 'hex'));
+  const message_digest = getDigest(Buffer.from(cbor_transaction, "hex"));
 
-  let signature = secp256k1.ecdsaSign(message_digest, child.privateKey);
+  const signature = secp256k1.ecdsaSign(message_digest, child.privateKey);
 
   // Concat v value at the end of the signature
-  let signatureRSV = Buffer.from(signature.signature).toString('hex') + Buffer.from([signature.recid]).toString('hex');
+  const signatureRSV =
+    Buffer.from(signature.signature).toString("hex") + Buffer.from([signature.recid]).toString("hex");
 
-  const response = await callMethod(
-    URL,
-    "verify_signature",
-    [signatureRSV, cbor_transaction],
-    1,
-  );
+  const response = await callMethod(URL, "verify_signature", [signatureRSV, cbor_transaction], 1);
 
   console.log(response);
 
@@ -218,28 +206,23 @@ test("verify_signature", async () => {
 });
 
 test("verify_invalid_signature", async () => {
-  let child = node.derivePath("m/44'/461'/0/0/0");
+  const child = node.derivePath("m/44'/461'/0/0/0");
 
-  let message_digest = getDigest(Buffer.from(cbor_transaction, 'hex'));
+  const message_digest = getDigest(Buffer.from(cbor_transaction, "hex"));
 
-  let signature = secp256k1.ecdsaSign(message_digest, child.privateKey);
+  const signature = secp256k1.ecdsaSign(message_digest, child.privateKey);
 
   // Tampered signature
-  let invalid_signature = Buffer.concat([Buffer.from(signature.signature).slice(0,36), Buffer.alloc(28)]);
+  const invalid_signature = Buffer.concat([Buffer.from(signature.signature).slice(0, 36), Buffer.alloc(28)]);
 
   // Concat recovery id value at the end of the signature
-  let signatureRSV = invalid_signature.toString('hex') + Buffer.from([signature.recid]).toString('hex');
+  const signatureRSV = invalid_signature.toString("hex") + Buffer.from([signature.recid]).toString("hex");
 
-  const response = await callMethod(
-    URL,
-    "verify_signature",
-    [signatureRSV, cbor_transaction],
-    1,
-  );
+  const response = await callMethod(URL, "verify_signature", [signatureRSV, cbor_transaction], 1);
 
   console.log(response);
 
-  let result = secp256k1.ecdsaVerify(invalid_signature, message_digest, child.publicKey);
+  const result = secp256k1.ecdsaVerify(invalid_signature, message_digest, child.publicKey);
 
   expect(result).toEqual(false);
   expect(response.result).toEqual(false);
@@ -265,7 +248,7 @@ test("verify_invalid_signature", async () => {
 // });
 
 test("get_status fail", async () => {
-  let message_cid = "bafy2bzaceaxm23epjsmh75yvzcecsrbavlmkcxnva66bkdebdcnyw3bjrc74u";
+  const message_cid = "bafy2bzaceaxm23epjsmh75yvzcecsrbavlmkcxnva66bkdebdcnyw3bjrc74u";
   const response = await callMethod(URL, "get_status", [message_cid], 1);
   console.log(response);
 
@@ -288,33 +271,31 @@ test("get_status fail", async () => {
 //   expect(response.result).toBeGreaterThanOrEqual(2);
 // });
 test("send_signed_tx", async () => {
-
-  let account = "t1lv32q33y64xs64pnyn6om7ftirax5ikspkumwsa";
+  const account = "t1lv32q33y64xs64pnyn6om7ftirax5ikspkumwsa";
 
   // Get Nonce
-  // FIXME: not public!
-  const nonce_response = await callMethod(
+  const nonceResponse = await callMethod(
     "http://86.192.13.13:1234/rpc/v0",
     "Filecoin.MpoolGetNonce",
     [account],
     1,
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.xK1G26jlYnAEnGLJzN1RLywghc4p4cHI6ax_6YOv0aI"
-  )
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.xK1G26jlYnAEnGLJzN1RLywghc4p4cHI6ax_6YOv0aI",
+  );
 
-  console.log(nonce_response.result)
+  console.log(nonceResponse.result);
 
   // Sign message
-  let child = node.derivePath("m/44'/461'/0/0/0");
+  const child = node.derivePath("m/44'/461'/0/0/0");
 
   const transaction = {
-    "to": "t17uoq6tp427uzv7fztkbsnn64iwotfrristwpryy",
-    "from": account,
-    "nonce": nonce_response.result,
-    "value": "1",
-    "gas_price": "0",
-    "gas_limit": "0",
-    "method": 0,
-    "params": ""
+    to: "t17uoq6tp427uzv7fztkbsnn64iwotfrristwpryy",
+    from: account,
+    nonce: nonceResponse.result,
+    value: "1",
+    gas_price: "0",
+    gas_limit: "0",
+    method: 0,
+    params: "",
   };
 
   const signature_response = await callMethod(
@@ -325,42 +306,37 @@ test("send_signed_tx", async () => {
   );
 
   const signature_hex = signature_response.result;
-  const signature_buf = Buffer.from(signature_hex,'hex');
-  const signature_base64 = signature_buf.toString('base64');
+  const signature_buf = Buffer.from(signature_hex, "hex");
+  const signature_base64 = signature_buf.toString("base64");
 
   console.log("Signature Hex String :", signature_hex);
   // convert DER + V signature into RSV
   // 30d07c692357b1728e988331a0cacc63396af73a3f19012350d2407c399b115b2b241b2a5bbdb5571a1cf57da81f3269bac31154f64bb83bc922804dc90718aa01
-  const r = signature_buf.slice(0,32);
-  const s = signature_buf.slice(32,64);
+  const r = signature_buf.slice(0, 32);
+  const s = signature_buf.slice(32, 64);
 
-  console.log("r :", r.toString('hex'))
-  console.log("s :", s.toString('hex'))
+  console.log("r :", r.toString("hex"));
+  console.log("s :", s.toString("hex"));
 
   console.log("Signature Base64     :", signature_base64);
 
-  let signed_tx = {
-    "message":{
-      "to":"t17uoq6tp427uzv7fztkbsnn64iwotfrristwpryy",
-      "from": account,
-      "nonce":nonce_response.result,
-      "value":"1",
-      "gas_price":"0",
-      "gas_limit":"0",
-      "method":0,
-      "params":""
+  const signed_tx = {
+    message: {
+      to: "t17uoq6tp427uzv7fztkbsnn64iwotfrristwpryy",
+      from: account,
+      nonce: nonceResponse.result,
+      value: "1",
+      gas_price: "0",
+      gas_limit: "0",
+      method: 0,
+      params: "",
     },
-    "signature": signature_base64
+    signature: signature_base64,
   };
 
-  const response = await callMethod(
-    URL,
-    "send_signed_tx",
-    [signed_tx],
-    1,
-  );
+  const response = await callMethod(URL, "send_signed_tx", [signed_tx], 1);
 
-  console.log(response)
+  console.log(response);
 
   expect(response).toHaveProperty("result");
 });
