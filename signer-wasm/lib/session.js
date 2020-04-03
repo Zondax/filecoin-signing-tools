449eed26607fc0c8aea64c3ec54e4fe918201b31
rllola <rllola80@gmail.com>
2020-04-03 14:53:54 +0200

    Just pass transport as an argument because;

--- signer-wasm/lib/session.js
+++ signer-wasm/lib/session.js
@@ -1,6 +1,5 @@
 import { DeviceNotSupported, UnknownDevice } from './errors';
 import FilecoinApp from "@zondax/ledger-filecoin"
-import TransportNodeHid from "@ledgerhq/hw-transport-node-hid";
 
 const DeviceEnum = {
   LEDGER: 'ledger',
@@ -10,15 +9,11 @@ const DeviceEnum = {
 
 // Utility class to hold hardware device session
 class DeviceSession {
-  constructor (device) {
+  constructor (device, transport) {
     this.device = device;
-    this.session = null
-  }
 
-  async connect () {
     switch (this.device) {
       case DeviceEnum.LEDGER:
-        const transport = await TransportNodeHid.create();
         this.session = new FilecoinApp(transport);
         break;
       case DeviceEnum.TREZOR:
