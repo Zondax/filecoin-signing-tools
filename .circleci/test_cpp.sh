2c8a5ae2b685db688f8d03601e295067db6ad509
Caio <c410.f3r@gmail.com>
2020-04-08 22:09:05 -0300

    Fix CI

--- .circleci/test_cpp.sh
+++ .circleci/test_cpp.sh
@@ -3,7 +3,6 @@ mkdir $OUT_DIR
 cp -r ../examples/ffi/c++/* $OUT_DIR
 rustup default nightly
 cbindgen --config cbindgen.toml --crate filecoin-signer-ffi --lang c++ --output $OUT_DIR/filecoin_signer_ffi.h
-rustup default stable
 cargo build
 cp ../target/debug/libfilecoin_signer_ffi.so $OUT_DIR
 ( cd $OUT_DIR; g++ main.cpp -L. -lfilecoin_signer_ffi -o main )
