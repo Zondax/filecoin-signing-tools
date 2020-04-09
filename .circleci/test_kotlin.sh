eb20861620a44e0121d822e6a69e89261856ef9d
Caio <c410.f3r@gmail.com>
2020-04-09 15:39:35 -0300

    Add Swift example

--- .circleci/test_kotlin.sh
+++ .circleci/test_kotlin.sh
@@ -9,7 +9,6 @@ cp -r ./signer-ffi/java/* $OUT_DIR
 cd $OUT_DIR
 cp /tmp/filecoin_signer_ffi.h .
 cp /tmp/libfilecoin_signer_ffi_java.so ./libfilecoin_signer_ffi.so
-javac -h . ./java/src/main/java/ch/zondax/FilecoinSigner.java 
-javac -d . ./src/main/java/ch/zondax/FilecoinSigner.java
+javac -d . -h . ./src/main/java/ch/zondax/FilecoinSigner.java 
 kotlinc -cp . ./Main.kt -include-runtime -d .
 kotlin -Djava.library.path="." -J-ea MainKt
