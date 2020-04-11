ffe252683dcaaa2382179be2890c9eb1067a275d
Juan Leni <lenijuan@gmail.com>
2020-04-11 16:18:18 +0200

    fix wasm unit tests

--- signer-wasm/src/lib.rs
+++ signer-wasm/src/lib.rs
@@ -200,7 +200,7 @@ mod tests {
             "nonce": 1,
             "value": "100000",
             "gasprice": "2500",
-            "gaslimit": "25000",
+            "gaslimit": 25000,
             "method": 0,
             "params": ""
         }"#;
