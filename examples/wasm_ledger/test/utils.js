c7158c9f3d039a1e6c793c7ddcc3e881e75f4546
rllola <rllola80@gmail.com>
2020-04-21 13:36:46 +0200

    wip

---
+++ examples/wasm_ledger/test/utils.js
@@ -0,0 +1,19 @@
+const blake2 = require("blake2");
+
+const CID_PREFIX = Buffer.from([0x01, 0x71, 0xa0, 0xe4, 0x02, 0x20]);
+
+function getCID(message) {
+    const hasher = blake2.createHash("blake2b", {digestLength: 32});
+    hasher.update(message);
+    return Buffer.concat([CID_PREFIX, hasher.digest()]);
+}
+
+function getDigest(message) {
+  // digest = blake2-256( prefix + blake2b-256(tx) )
+
+  const hasher = blake2.createHash("blake2b", { digestLength: 32 });
+  hasher.update(getCID(message));
+  return hasher.digest();
+}
+
+module.exports = { getCID, getDigest }
