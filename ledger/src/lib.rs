c78e5205396656480266b3eeaa650ebab8cada89
Juan Leni <lenijuan@gmail.com>
2020-05-06 10:32:27 +0200

    removing dependency

--- ledger/src/lib.rs
+++ ledger/src/lib.rs
@@ -29,7 +29,7 @@ mod params;
 use self::params::{APDUErrors, PayloadType};
 use crate::params::*;
 
-use ledger_generic::{ApduAnswer, ApduCommand};
+pub use ledger_generic::{ApduAnswer, ApduCommand};
 use std::str;
 
 mod errors;
