a2cc7a9238fe6f6495bcfc02cbe6870bba54ee61
rllola <rllola80@gmail.com>
2020-06-25 16:16:08 +0200

    Remove CommonJS syntax;

--- examples/wasm_ledger/test/utils.js
+++ examples/wasm_ledger/test/utils.js
@@ -2,14 +2,14 @@ const blake = require('blakejs');
 
 const CID_PREFIX = Buffer.from([0x01, 0x71, 0xa0, 0xe4, 0x02, 0x20]);
 
-export function getCID(message) {
+function getCID(message) {
   const blakeCtx = blake.blake2bInit(32);
   blake.blake2bUpdate(blakeCtx, message);
   const hash = blake.blake2bFinal(blakeCtx);
   return Buffer.concat([CID_PREFIX, hash]);
 }
 
-export function getDigest(message) {
+function getDigest(message) {
   // digest = blake2-256( prefix + blake2b-256(tx) )
 
   const blakeCtx = blake.blake2bInit(32);
