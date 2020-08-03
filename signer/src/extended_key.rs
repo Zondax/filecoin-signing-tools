6523ece6a03203700803de59caca22663b796919
rllola <rllola80@gmail.com>
2020-08-03 13:42:09 +0200

    rustfmt

--- signer/src/extended_key.rs
+++ signer/src/extended_key.rs
@@ -2,7 +2,7 @@ use secp256k1::util::{COMPRESSED_PUBLIC_KEY_SIZE, FULL_PUBLIC_KEY_SIZE, SECRET_K
 use secp256k1::{PublicKey, SecretKey};
 
 use crate::error::SignerError;
-use hmac::{Hmac, NewMac, Mac};
+use hmac::{Hmac, Mac, NewMac};
 use sha2::Sha512;
 use std::convert::TryFrom;
 use std::fmt;
