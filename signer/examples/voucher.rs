444706cd6aead596d474af641da72679b37a78c8
rllola <rllola80@gmail.com>
2020-08-31 18:42:50 +0200

    rustfmt

--- signer/examples/voucher.rs
+++ signer/examples/voucher.rs
@@ -1,16 +1,17 @@
 use extras::paych::SignedVoucher;
+use filecoin_signer::{utils, PrivateKey};
 use forest_address::Address;
-use num_bigint_chainsafe::BigInt;
-use std::str::FromStr;
 use forest_encoding::{from_slice, to_vec};
+use num_bigint_chainsafe::BigInt;
 use secp256k1::{recover, sign, verify, Message, RecoveryId};
 use std::convert::TryFrom;
-use filecoin_signer::{PrivateKey, utils};
+use std::str::FromStr;
 
 fn main() {
-    let private_key = PrivateKey::try_from("YbDPh1vq3fBClzbiwDt6WjniAdZn8tNcCwcBO2hDwyk=".to_string()).unwrap();
+    let private_key =
+        PrivateKey::try_from("YbDPh1vq3fBClzbiwDt6WjniAdZn8tNcCwcBO2hDwyk=".to_string()).unwrap();
     let secret_key = secp256k1::SecretKey::parse_slice(&private_key.0).unwrap();
-    
+
     let v = SignedVoucher {
         channel_addr: Address::from_str("t2h6o4uvzsksf3yi2ri2uu7eqvhqkcp7axmg3mski").unwrap(),
         time_lock_min: 1234,
@@ -28,7 +29,7 @@ fn main() {
     /*println!("{:?}", v.signing_bytes().unwrap());*/
     println!("{:?}", v);
     println!("{:?}", base64::encode(to_vec(&v).unwrap()));
-    
+
     let svb = v.signing_bytes().unwrap();
     let digest = utils::get_digest(&svb).unwrap();
 
@@ -37,7 +38,7 @@ fn main() {
     let (signature_rs, recovery_id) = sign(&blob_to_sign, &secret_key);
 
     println!("{:?}", base64::encode(signature_rs.serialize().to_vec()));
-    
+
     let decoded_voucher = base64::decode(base64::encode(to_vec(&v).unwrap())).unwrap();
     let mut voucher: SignedVoucher = from_slice(&decoded_voucher).unwrap();
 
