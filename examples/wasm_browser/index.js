04ea361ad56023b236e0511bcaddcf3733d8f242
rllola <rllola80@gmail.com>
2021-06-30 13:45:46 +0200

    added pure js example in browser

--- examples/wasm_browser/index.js
+++ examples/wasm_browser/index.js
@@ -1,4 +1,5 @@
-import * as wasm from '@zondax/filecoin-signing-tools/js'	//import * as wasm from "@zondax/filecoin-signing-tools/js";
+import * as wasm from '@zondax/filecoin-signing-tools'
+import * as js from '@zondax/filecoin-signing-tools/js'
 
 function log(text) {
   document.getElementById('output').innerHTML += text + '\n'
@@ -56,13 +57,63 @@ const unsigned_tx = {
 }
 
 console.log('About to call wasm.transactionSignLotus():')
-wasm.transactionSignLotus(unsigned_tx, key.private_hexstring)
+wasm.transactionSignLotus(unsigned_tx, key.private_raw)
 console.log('Done calling wasm.transactionSignLotus()')
 
 log(`unsigned_tx = ${JSON.stringify(unsigned_tx, 0, 4)}`)
 
-let signed_tx = wasm.transactionSign(unsigned_tx, key.private_hexstring)
+let signed_tx = wasm.transactionSign(unsigned_tx, key.private_raw)
 
 log('\n...sign...\n')
 log(`signed_tx = ${JSON.stringify(signed_tx, 0, 4)}`)
 
+// PURE JS LIB
+
+/////////////////////////////////
+// Generate Mnemonic
+
+mnemonic = js.generateMnemonic()
+log('<h2>[js.mnemonic_generate]</h2>' + mnemonic)
+log('mnemonic')
+
+/////////////////////////////////
+// Derive key
+
+key = js.keyDerive(mnemonic, 'm/44\'/461\'/0/0/0', '')
+
+log('<h2>[js.key_derive]</h2>')
+log(`<b>address      </b> ${key.address}`)
+log(`<b>public  key  </b> ${key.public_hexstring}`)
+log(`<b>private key  </b> ${key.private_hexstring}`)
+log(`<b>public array </b> ${key.public_raw}`)
+log(`<b>private array</b> ${key.private_raw}`)
+log(`<b>public base64</b> ${key.public_base64}`)
+log(`<b>private base64</b> ${key.private_base64}`)
+
+/////////////////////////////////
+// Recover key
+
+recovered_key = js.keyRecover(Buffer.from('6a1a68774457742a8bc69db5491df5ae7677687d49e1003a78e2d60959d5f7a7',"hex"))
+
+log('<h2>[js.key_recover]</h2>')
+log(`<b>address      </b> ${recovered_key.address}`)
+log(`<b>public  key  </b> ${recovered_key.public_hexstring}`)
+log(`<b>private key  </b> ${recovered_key.private_hexstring}`)
+log(`<b>public array </b> ${recovered_key.public_raw}`)
+log(`<b>private array</b> ${recovered_key.private_raw}`)
+
+/////////////////////////////////
+// Sign transaction
+
+log('<h2>[js.sign_transaction]</h2>')
+
+console.log('About to call js.transactionSignLotus():')
+js.transactionSignLotus(unsigned_tx, key.private_raw)
+console.log('Done calling js.transactionSignLotus()')
+
+log(`unsigned_tx = ${JSON.stringify(unsigned_tx, 0, 4)}`)
+
+signed_tx = js.transactionSign(unsigned_tx, key.private_raw)
+
+log('\n...sign...\n')
+log(`signed_tx = ${JSON.stringify(signed_tx, 0, 4)}`)
