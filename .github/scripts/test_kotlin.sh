9b390cb2be50c25f7059260b0eba6915b9addde2
Emmanuel <emmanuelm41@gmail.com>
2021-07-13 11:23:29 -0300

    add more jobs to github actions

---
+++ .github/scripts/test_kotlin.sh
@@ -0,0 +1,14 @@
+wget -O sdk.install.sh "https://get.sdkman.io"
+bash sdk.install.sh
+. ~/.sdkman/bin/sdkman-init.sh
+sdk install kotlin
+
+mkdir $OUT_DIR
+cp -r ./examples/ffi/kotlin/* $OUT_DIR
+cp -r ./signer-ffi/java/* $OUT_DIR
+cd $OUT_DIR
+cp /tmp/filecoin_signer_ffi.h .
+cp /tmp/libfilecoin_signer_ffi_java.so ./libfilecoin_signer_ffi.so
+javac -d . -h . ./src/main/java/ch/zondax/FilecoinSigner.java 
+kotlinc -cp . ./Main.kt -include-runtime -d .
+kotlin -Djava.library.path="." -J-ea MainKt
