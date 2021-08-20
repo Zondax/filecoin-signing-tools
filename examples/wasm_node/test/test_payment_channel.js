dd239b2b0575830c939dc703d977fa74cc6c0409
Anonyma <zxenonwolf@gmail.com>
2021-08-20 19:32:25 +0200

    Retrieve Cid in CI payment channel test

--- examples/wasm_node/test/test_payment_channel.js
+++ examples/wasm_node/test/test_payment_channel.js
@@ -78,6 +78,31 @@ describeCall('createPymtChan', function() {
     let signedMessage = filecoin_signer.transactionSignLotus(create_pymtchan, paymentchannel_create.private_key)
     signedMessage = JSON.parse(signedMessage)
 
+    try {
+      const messageForCid = {
+        "message": {
+          "to": signedMessage.Message.To,
+          "from": signedMessage.Message.From,
+          "nonce": +signedMessage.Message.Nonce,
+          "value": signedMessage.Message.Value,
+          "gas_limit": +signedMessage.Message.GasLimit,
+          "gas_fee_cap": signedMessage.Message.GasFeeCap,
+          "gas_premium": signedMessage.Message.GasPremium,
+          "method": +signedMessage.Message.Method,
+          "params": signedMessage.Message.Params
+        },
+        "signature": {
+          "type": signedMessage.Signature.Type,
+          "data": signedMessage.Signature.Data
+        }
+      }
+      console.log('messageForCid: ', messageForCid);
+      const cid = filecoin_signer.getCid(messageForCid);
+      console.log('cid: ', cid);
+    } catch (error) {
+      console.log('getCid error: ', error)
+    }
+
     assert.deepStrictEqual(paymentchannel_create.message.params, create_pymtchan.params)
 
     // TODO: verify signature
