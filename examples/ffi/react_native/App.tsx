cd30527602e6334b7ee4c69af54b50c77d20ee27
Prateek Rathod <prateek@zondax.ch>
2022-07-14 21:50:37 +0530

    feat: react native example init for key_derive

---
+++ examples/ffi/react_native/App.tsx
@@ -0,0 +1,86 @@
+import { StatusBar } from 'expo-status-bar'
+import { useEffect, useState } from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+// @ts-ignore
+import { generateMnemonic, keyDerive } from '@zondax/filecoin-signing-tools/js'
+
+export default function App() {
+  const [mnemonic, setMnemonic] = useState(generateMnemonic())
+  const [derivationPath, setDerivationPath] = useState("m/44'/461'/0/0/1")
+  const [dpInvalid, setDpInvalid] = useState(false)
+  const [account, setAccount] = useState({ private_base64: '', address: '' })
+
+  useEffect(() => {
+    try {
+      setAccount(keyDerive(mnemonic, derivationPath, ''))
+    } catch (e) {
+      console.log(e)
+    }
+  }, [mnemonic, derivationPath])
+
+  return (
+    <View style={styles.container}>
+      <Text>
+        Mnemonic Seed:
+        <br />
+      </Text>
+      <TextInput
+        // @ts-ignore
+        style={styles.textInput}
+        onChange={e => {
+          setMnemonic(e.nativeEvent.text)
+        }}
+        value={mnemonic}
+      />
+      <Text>
+        Derivation path:
+        <br />
+      </Text>
+      <TextInput
+        // @ts-ignore
+        style={styles.textInput}
+        onChange={e => {
+          setDerivationPath(e.nativeEvent.text)
+        }}
+        value={derivationPath}
+      />
+      <Text>
+        Address:
+        <br />
+        {account.address}
+      </Text>
+
+      <Text>
+        KeyFile (to add account in Lotus):
+        <br />
+        {Buffer.from(`{"Type":"secp256k1","PrivateKey":"${account.private_base64}"}`).toString('hex')}
+      </Text>
+      <Button
+        title="Refresh"
+        color="#454545"
+        onPress={() => {
+          setMnemonic(generateMnemonic())
+          setDerivationPath("m/44'/461'/0/0/0")
+          console.log('Refresh')
+        }}
+      />
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  textInput: {
+    width: '400px',
+    border: '1px solid black',
+    borderRadius: 3,
+    fontSize: '16px',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    rowGap: '1rem',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
---
+++ examples/ffi/react_native/utils.ts
@@ -0,0 +1,27 @@
+import Storage from 'react-native-storage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const storage = new Storage({
+  // maximum capacity, default 1000 key-ids
+  size: 1000,
+
+  // Use AsyncStorage for RN apps, or window.localStorage for web apps.
+  // If storageBackend is not set, data will be lost after reload.
+  storageBackend: window.localStorage, // for web: window.localStorage
+
+  // expire time, default: 1 day (1000 * 3600 * 24 milliseconds).
+  // can be null, which means never expire.
+  defaultExpires: 1000 * 3600 * 24,
+
+  // cache data in the memory. default is true.
+  enableCache: true,
+
+  // if data was not found in storage or expired data was found,
+  // the corresponding sync method will be invoked returning
+  // the latest data.
+  sync: {
+    // we'll talk about the details later.
+  },
+})
+
+export default storage
