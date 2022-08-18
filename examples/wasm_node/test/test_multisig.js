b129c2be72048e77e3a8292e9d78395cc01b2de1
rllola <rllola80@gmail.com>
2022-08-18 17:50:46 +0200

    update multisig tests

--- examples/wasm_node/test/test_multisig.js
+++ examples/wasm_node/test/test_multisig.js
@@ -38,18 +38,12 @@ describeCall('createMultisig', function() {
       StartEpoch: multisig_create.constructor_params["StartEpoch"]
     }
 
-    console.log(constructor_params)
-
     let params = {
       CodeCid: 'bafk2bzacebhldfjuy4o5v7amrhp5p2gzv2qo5275jut4adnbyp56fxkwy5fag',
       ConstructorParams: Buffer.from(filecoin_signer.serializeParams(constructor_params)).toString('base64')
     }
 
-    console.log(params)
-
     let serialized_params = filecoin_signer.serializeParams(params);
-
-    console.log(Buffer.from(serialized_params).toString('base64'))
 
     let create_multisig_transaction = {
       To: multisig_create.message["To"],
@@ -63,27 +57,22 @@ describeCall('createMultisig', function() {
       Params: Buffer.from(serialized_params).toString('base64')
     }
 
-    /*let create_multisig_transaction = filecoin_signer.createMultisigWithFee(
-      multisig_create.message["From"],
-      multisig_create.constructor_params["Signers"],
-      multisig_create.message["Value"],
-      multisig_create.constructor_params["NumApprovalsThreshold"],
-      multisig_create.message["Nonce"],
-      multisig_create.constructor_params["UnlockDuration"].toString(),
-      multisig_create.constructor_params["StartEpoch"].toString(),
-      multisig_create.message["GasLimit"].toString(),
-      multisig_create.message["GasFeeCap"],
-      multisig_create.message["GasPremium"],
-      "mainnet"
-    )*/
-
     assert.deepStrictEqual(create_multisig_transaction, multisig_create.message)
   })
 
   it('should return a create multisig transaction with duration -1', function() {
     const multisig_create = dataTxs.create
 
-    let create_multisig_transaction = filecoin_signer.createMultisigWithFee(
+    let constructor_params = { 
+      Signers: multisig_create.constructor_params["Signers"],
+      NumApprovalsThreshold: multisig_create.constructor_params["NumApprovalsThreshold"],
+      UnlockDuration: BigInt(-1).toString(),
+      StartEpoch: multisig_create.constructor_params["StartEpoch"]
+    }
+
+    filecoin_signer.serializeParams(constructor_params)
+
+    /*let create_multisig_transaction = filecoin_signer.createMultisigWithFee(
       multisig_create.message["From"],
       multisig_create.constructor_params["Signers"],
       multisig_create.message["Value"],
@@ -95,7 +84,7 @@ describeCall('createMultisig', function() {
       multisig_create.message["GasFeeCap"],
       multisig_create.message["GasPremium"],
       "mainnet"
-    )
+    )*/
 
     assert(create_multisig_transaction)
   })
@@ -124,32 +113,49 @@ describeCall('createMultisig', function() {
   it('should fail because of bigint', function() {
     const multisig_create = dataTxs.create
 
+    let constructor_params = { 
+      Signers: multisig_create.constructor_params["Signers"],
+      NumApprovalsThreshold: multisig_create.constructor_params["NumApprovalsThreshold"],
+      UnlockDuration: '18446744073709551617',
+      StartEpoch: multisig_create.constructor_params["StartEpoch"]
+    }
+
+    filecoin_signer.serializeParams(constructor_params)
+
     assert.throws(
-      () => filecoin_signer.createMultisigWithFee(
-        multisig_create.message["From"],
-        multisig_create.constructor_params["Signers"],
-        multisig_create.message["Value"],
-        multisig_create.constructor_params["NumApprovalsThreshold"],
-        multisig_create.message["Nonce"],
-        multisig_create.constructor_params["UnlockDuration"].toString(),
-        multisig_create.constructor_params["StartEpoch"].toString(),
-        '18446744073709551617',
-        multisig_create.message["GasFeeCap"],
-        multisig_create.message["GasPremium"],
-        "mainnet"
-      ),
+      () => filecoin_signer.serializeParams(constructor_params),
       /(number too large to fit in target type)/,
     )
 
   })
-
 })
 
 describeCall('proposeMultisig', function() {
   it('should return a propose multisig transaction', function() {
     const multisig_propose = dataTxs.propose
 
-    let propose_multisig_transaction = filecoin_signer.proposeMultisigWithFee(
+    let propose_params = { 
+      To: multisig_propose.proposal_params["To"],
+      Value: multisig_propose.proposal_params["Value"],
+      Method: multisig_propose.proposal_params["Method"],
+      Params: multisig_propose.proposal_params["Params"]
+    }
+
+    let serialized_params = filecoin_signer.serializeParams(propose_params);
+
+    let propose_multisig_transaction = {
+      To: multisig_propose.message["To"],
+      From: multisig_propose.message["From"],
+      Nonce: multisig_propose.message["Nonce"],
+      Value: multisig_propose.message["Value"],
+      GasLimit: multisig_propose.message["GasLimit"],
+      GasFeeCap: multisig_propose.message["GasFeeCap"],
+      GasPremium: multisig_propose.message["GasPremium"],
+      Method: multisig_propose.message["Method"],
+      Params: Buffer.from(serialized_params).toString('base64')
+    }
+
+    /*let propose_multisig_transaction = filecoin_signer.proposeMultisigWithFee(
       multisig_propose.message["To"],
       multisig_propose.proposal_params["To"],
       multisig_propose.message["From"],
@@ -160,7 +166,7 @@ describeCall('proposeMultisig', function() {
       multisig_propose.message["GasPremium"],
       multisig_propose.proposal_params["Method"],
       multisig_propose.proposal_params["Params"],
-    )
+    )*/
 
     assert.deepStrictEqual(multisig_propose.message, propose_multisig_transaction)
   })
@@ -191,7 +197,33 @@ describeCall('approveMultisig', function() {
   it('should return an approval multisig transaction', function() {
     const multisig_approve = dataTxs.approve
 
-    let approve_multisig_transaction = filecoin_signer.approveMultisigWithFee(
+    let proposal_params = { 
+      To: multisig_approve.approval_params["To"],
+      Value: multisig_approve.approval_params["Value"],
+      Method: multisig_approve.approval_params["Method"],
+      Params: multisig_approve.approval_params["Params"]
+    }
+
+    let approve_params = {
+      Id: multisig_approve.approval_params["TxnID"],
+      Proposal: Buffer.from(filecoin_signer.serializeParams(proposal_params)).toString('base64')
+    }
+
+    let serialized_params = filecoin_signer.serializeParams(approve_params);
+
+    let approve_multisig_transaction = {
+      To: multisig_approve.message["To"],
+      From: multisig_approve.message["From"],
+      Nonce: multisig_approve.message["Nonce"],
+      Value: multisig_approve.message["Value"],
+      GasLimit: multisig_approve.message["GasLimit"],
+      GasFeeCap: multisig_approve.message["GasFeeCap"],
+      GasPremium: multisig_approve.message["GasPremium"],
+      Method: multisig_approve.message["Method"],
+      Params: Buffer.from(serialized_params).toString('base64')
+    }
+
+    /*let approve_multisig_transaction = filecoin_signer.approveMultisigWithFee(
       multisig_approve.message["To"],
       multisig_approve.approval_params["TxnID"],
       multisig_approve.proposal_params["Requester"],
@@ -202,7 +234,7 @@ describeCall('approveMultisig', function() {
       multisig_approve.message["GasLimit"].toString(),
       multisig_approve.message["GasFeeCap"],
       multisig_approve.message["GasPremium"],
-    )
+    )*/
 
     assert.deepStrictEqual(multisig_approve.message, approve_multisig_transaction)
   })
@@ -232,7 +264,33 @@ describeCall('cancelMultisig', function() {
   it('should return a cancel multisig transaction', function() {
     const multisig_cancel = dataTxs.cancel
 
-    let cancel_multisig_transaction = filecoin_signer.cancelMultisigWithFee(
+    let proposal_params = { 
+      To: multisig_cancel.cancel_params["To"],
+      Value: multisig_cancel.cancel_params["Value"],
+      Method: multisig_cancel.cancel_params["Method"],
+      Params: multisig_cancel.cancel_params["Params"]
+    }
+
+    let cancel_params = {
+      Id: multisig_cancel.cancel_params["TxnID"],
+      Proposal: Buffer.from(filecoin_signer.serializeParams(proposal_params)).toString('base64')
+    }
+
+    let serialized_params = filecoin_signer.serializeParams(cancel_params);
+
+    let cancel_multisig_transaction = {
+      To: multisig_cancel.message["To"],
+      From: multisig_cancel.message["From"],
+      Nonce: multisig_cancel.message["Nonce"],
+      Value: multisig_cancel.message["Value"],
+      GasLimit: multisig_cancel.message["GasLimit"],
+      GasFeeCap: multisig_cancel.message["GasFeeCap"],
+      GasPremium: multisig_cancel.message["GasPremium"],
+      Method: multisig_cancel.message["Method"],
+      Params: Buffer.from(serialized_params).toString('base64')
+    }
+
+    /*let cancel_multisig_transaction = filecoin_signer.cancelMultisigWithFee(
       multisig_cancel.message["To"],
       multisig_cancel.cancel_params["TxnID"],
       multisig_cancel.proposal_params["Requester"],
@@ -243,7 +301,7 @@ describeCall('cancelMultisig', function() {
       multisig_cancel.message["GasLimit"].toString(),
       multisig_cancel.message["GasFeeCap"],
       multisig_cancel.message["GasPremium"],
-    )
+    )*/
 
     assert.deepStrictEqual(multisig_cancel.message, cancel_multisig_transaction)
   })
